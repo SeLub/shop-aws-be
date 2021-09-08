226041782b03b7a19d18797c3b64970572631c15
SeLub <siteup24@gmail.com>
2021-09-09 02:01:14 +0300

    refactor: add console log and some error handling

--- product-service/functions/createProduct/createProduct.js
+++ product-service/functions/createProduct/createProduct.js
@@ -28,41 +28,48 @@ let data_export = {}, error_code = 200;
 
 export const handler = async event => {
 
+  console.log(event);
+
   const client = new Client(credentials);
+
+  client.on('error', err => {
+                            data_export = 'DB Client Error 500:' + err.stack;
+                            error_code = 500;
+                            console.error('DB Client Error 500:', err.stack);
+                            })
 
   await client
           .connect()
           .then(() => console.log('Client connected'))
-          .catch(err => {data_export = 'DB Error in client connection:' + err.stack; error_code = 500})
-
-const { title, description, price, image_id, count } = JSON.parse(event.body)
-
-if (!title) {title=''; data_export = 'Error: Title is mandatory! Please provide Title of Product.'; error_code = 500}
-if (!description) {description = ''}
-if (!price) {price = 0}
-if (!image_id) {image_id = 'null'}
-if (!count) {count = 0}
+          .catch(err => {data_export = 'DB connection error:' + err.stack; error_code = 500})
 
 
+  const { title, description, price, imageid, count } = JSON.parse(event.body)
 
-let insertQuery = 'WITH rows AS(\
-                   INSERT INTO products (title, description, price, imageid)\
-                   VALUES (title, description, price, image_id)\
-                   RETURNING id)\
-                   INSERT INTO stocks (product_id, count)\
-                   VALUES ((SELECT id FROM rows), 12);'
+  if (!title) {title=''; data_export = 'Error: Title is mandatory! Please provide Title of Product.'; error_code = 500}
+  if (!description) {description = ''}
+  if (!price) {price = 0}
+  if (!imageid) {imageid = 'null'}
+  if (!count) {count = 0}
 
 
+
+  let insertQuery = 'WITH rows AS(\
+                     INSERT INTO products (title, description, price, imageid)\
+                     VALUES (title, description, price, image_id)\
+                     RETURNING id)\
+                     INSERT INTO stocks (product_id, count)\
+                     VALUES ((SELECT id FROM rows), 12);'
 
  await client
           .query(insertQuery)
           .then(res => { data_export = res })
-          .catch(err => { data_export = 'DB Error in Query 500:' + err.stack; error_code = 500})   
+          .catch(err => { data_export = 'DB query error 500:' + err.stack; error_code = 500})   
   
   await client
           .end()
           .then(() => console.log('Client disconnected'))
-          .catch(err => {data_export = 'DB Error in disconnection 500:' + err.stack; error_code = 500})
+          .catch(err => {data_export = 'DB disconnection error 500:' + err.stack; error_code = 500})
 
   return await handleResponse({data_export, error_code});
 }
