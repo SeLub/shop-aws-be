dada87d17bad12fa8a01813a151c35d5aca88296
SeLub <siteup24@gmail.com>
2021-09-08 21:29:08 +0300

    feat: getProductsById 400 Product not found

--- product-service/functions/getProductsById/getProductsById.js
+++ product-service/functions/getProductsById/getProductsById.js
@@ -50,5 +50,7 @@ export const handler = async event => {
           .then(() => console.log('Client disconnected'))
           .catch(err => {data_export = 'DB Error 500:' + err.stack; error_code = 500})
 
+  if (JSON.stringify(data_export) == JSON.stringify([])) { data_export = 'Product not found 400: Wrong id'; error_code = 400 }
+
   return await handleResponse({data_export, error_code});
 }
