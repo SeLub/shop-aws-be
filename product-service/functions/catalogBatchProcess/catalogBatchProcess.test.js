0cf6d9c3a783466d95d7d82b6a8285f5089400dd
SeLub <siteup24@gmail.com>
2021-10-08 23:46:35 +0300

    feat: add test for catalogBatchProcess

--- product-service/functions/catalogBatchProcess/catalogBatchProcess.test.js
+++ product-service/functions/catalogBatchProcess/catalogBatchProcess.test.js
@@ -1,78 +1,56 @@
-import AWSMock from 'aws-sdk-mock';
+import * as AWSMock from 'aws-sdk-mock';
 import AWS from 'aws-sdk';
+
+jest.mock('../../models/createNewProduct');
+
+import {createNewProduct} from '../../models/createNewProduct';
 import { handler as catalogBatchProcess } from './catalogBatchProcess';
-import { API_ERRORS } from '../../../common/constants';
+import { httpStatus } from '../../libs/httpStatus';
 
-const testRecord = {
-  title: 'Test title',
-  description: 'Test description',
-  count: 1,
-  image_id: '007',
-  price: 10,
-};
-
-jest.mock('pg', () => {
-  const mClient = {
-    connect: jest.fn(),
-    query: jest.fn(() => ({
-      rows: [{ ...testRecord, id: '001' }],
-    })),
-    end: jest.fn(),
-  };
-  return { Client: jest.fn(() => mClient) };
-});
+AWSMock.setSDKInstance(AWS)
 
 describe('catalogBatchProcess', () => {
-  process.env = {
-    SNS_ARN: 'test-sns-arn',
-    PG_USERNAME: 'user_name',
-    PG_HOST: 'host',
-    PG_DATABASE: 'data_base',
-    PG_PASSWORD: 'password',
-    PG_PORT: '5432',
-  };
 
-  const getEvent = record => ({
-    Records: [
-      {
-        body: JSON.stringify(record),
-      },
-    ],
-  });
+  const validData = {
+    Records :
+       [
+          {body:JSON.stringify({'title':'row1', description: 'desc1', price: 111, count: 1})},
+          {body:JSON.stringify({'title':'row2', description: 'desc2', price: 222, count: 2})},
+          {body:JSON.stringify({'title':'row3', description: 'desc3', price: 333, count: 3})},
+        ]
+  }
 
-  AWSMock.setSDKInstance(AWS);
+  it('should return call sns.publish and createNewProduct 3 times', async () => {
 
-  beforeEach(() => {
-    AWSMock.mock('SNS', 'publish', Promise.resolve('The data has been sended'));
-  });
+    // @ts-ignore
+    createNewProduct.mockImplementation((title, description, price, count) => 'ok');
 
-  afterEach(() => {
-    jest.clearAllMocks();
-    AWSMock.restore();
-  });
+    const snsPublishMock = jest.fn((opt, cb) => cb(opt))
 
-  test('publish the message', async () => {
-    const result = await catalogBatchProcess(getEvent(testRecord));
-    expect(result.body).toBe('The data has been sended');
-  });
+    AWSMock.mock('SNS', 'publish', snsPublishMock)
+    
+    await catalogBatchProcess(validData);
 
-  test('throws an error if not all params are correct', async () => {
-    const record = { ...testRecord };
-    delete record.count;
+    AWSMock.restore('SNS');
 
-    const result = await catalogBatchProcess(getEvent(record));
-    const { body, statusCode } = result;
+    expect(snsPublishMock).toHaveBeenCalledTimes(validData.Records.length);
 
-    expect(statusCode).toBe(400);
-    expect(body.message).toEqual(API_ERRORS.INVALID_QUERY_PARAMETRS);
-  });
+    expect(createNewProduct).toHaveBeenCalledTimes(validData.Records.length);
 
-  test('returns the error in case of any unhandled error', async () => {
-    AWSMock.remock('SNS', 'publish', Promise.reject(new Error(API_ERRORS.SOMETHING_WRONG)));
-    const result = await catalogBatchProcess(getEvent(testRecord));
-    const { body, statusCode } = result;
+  }); 
 
-    expect(statusCode).toBe(500);
-    expect(body.message).toEqual(API_ERRORS.SOMETHING_WRONG);
-  });
+
+   it('should return 500 error code if function raise error', async () => {
+
+    AWSMock.mock('SNS', 'publish', (_operation, _params) => { throw new Error('Test error'); });
+     
+     const result = await catalogBatchProcess(validData);
+
+     AWSMock.restore('SNS');
+
+     expect(result.statusCode).toBe(httpStatus.SERVER_ERROR);
+
+   }); 
+
+
 });
